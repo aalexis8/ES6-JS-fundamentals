23e3825647e72540d52f882fc57f15cd429a06e5
Alex Alexis <aalexis8@gmail.com>
2021-01-25 16:13:53 -0500

    console log, logging to the console saves lives

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-alert("Connected!");
+// Log to console
+console.log("Hello World!");
+console.log(123);
+console.log(true);
+var greeting = "What's up";
+console.log(greeting);
+console.log([1, 2, 3, 4, 5, 6]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 3 });
+
+/*
+  multi
+  line
+  comments
+ 
+*/
+console.error("This is where you fudge up");
+console.clear();
+console.warn("This is where you came close to fudging up");
+console.time("Hell No");
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+
+console.timeEnd("Hell No");
