430fbf84d1e217862a24e804716bf0b4e2c10a5f
Alex Alexis <aalexis8@gmail.com>
2021-01-25 16:48:06 -0500

    Intro to variables, var const and let

--- app.js
+++ app.js
@@ -1,26 +1,70 @@
-// Log to console
-console.log("Hello World!");
-console.log(123);
-console.log(true);
-var greeting = "What's up";
-console.log(greeting);
-console.log([1, 2, 3, 4, 5, 6]);
-console.log({ a: 1, b: 2 });
-console.table({ a: 1, b: 3 });
+// defining your variables --
+// three available keywords -- var, let, const
+// let and const are newer, var has quirks you need to keep in mind.
 
-/*
-  multi
-  line
-  comments
- 
-*/
-console.error("This is where you fudge up");
-console.clear();
-console.warn("This is where you came close to fudging up");
-console.time("Hell No");
-console.log(greeting);
-console.log(greeting);
-console.log(greeting);
-console.log(greeting);
+// creating a varibale
+// var name = "Alvin";
+// console.log(name);
+// name = "Kraven";
+// console.log(name);
 
-console.timeEnd("Hell No");
+// Initializing your vars, or else they are undefined.
+// var greeting;
+// console.log(greeting);
+// greeting = "What's good";
+// console.log(greeting);
+
+// rules for variable names -- letters, numbers, _, $
+// Cannot start with number
+// var _name = "Jason";
+
+// // Multi word vars , camel case
+// var firstName = "John"; // Camel Case
+// var first_name = "Conners"; // underscore
+// var FirstName = "Eddie";// Pascal case
+
+// Let and Scope, the block scoping variables
+// For now remember that let basically works like var --
+// We wont talk about scope yet.
+
+// LET
+// let name;
+// let name = "Alvin";
+// console.log(name);
+// name = "Kraven";
+// console.log(name);
+
+// CONST -- variables declared with const, can't be re-assigned a new value. They can't change value, once assigned. And they can't be declared without a value (can't do const name; needs a value)
+
+const name = "Anything even empty string";
+console.log(name);
+// cannot reassign
+// name = "Something new"; // not permitted
+// And require a value at declaration.
+//const greeting; //  Missing initializer in const declaration
+
+// Keep in mind value inside a const object can change.
+// Take this object literal
+const person = {
+  name: "Jason",
+  age: 200,
+};
+
+// We can change the value of the key name or age
+person.name = "Voorheese";
+person.age = 300;
+
+console.log(person); //
+
+const anotherperson = { name: "Freddy" };
+// can't assign person to a new or other person.
+// person = anotherperson;
+
+/* ---------- */
+const numbers = [1, 2, 3, 4, 5];
+// we can use the array methods to add items to this array
+numbers.push(6);
+console.log(numbers);
+
+// But we can't re-assign array to a new set of numbers
+numbers = [];
