5741358e3ade9905103eb9548528108df4845389
Alex Alexis <aalexis8@gmail.com>
2021-01-25 17:48:46 -0500

    Number & the Math object

--- app.js
+++ app.js
@@ -1,47 +1,31 @@
-// Taking a look at type conversion -- before we talk about coercion
+// Numbers & The Math Object
+
+const num1 = 100;
+const num2 = 50;
 
 let val;
 
-// Number to string
-//val = 5;
-val = String(534);
-val = String(4 + 5);
+// The basics
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+// val = num1 / num2;
+// val = num1 % num2;
+//val = num1 + num2;
 
-// Bool to string
-val = String(true);
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.7);
+val = Math.ceil(2.4);
+val = Math.floor(3.1);
+val = Math.sqrt(144);
+val = Math.abs(-4);
+val = Math.pow(6, 3);
+val = Math.max(2, 55, 34, 23, 67, 86);
+val = Math.min(2, 55, 34, 23, 67, 86);
 
-// Date to string
-val = String(new Date());
+val = Math.random();
 
-// Array to string
-val = String([1, 2, 3, 4]);
-
-// toString() -- what's the difference, if any?
-val = (3).toString();
-val = false.toString();
-
-// String number
-val = Number("5");
-val = Number(true);
-val = Number(false);
-val = Number(null);
-val = Number("hello");
-val = Number([3, 4, 5, 6, 8]);
-
-val = parseInt("100");
-val = parseFloat("132.34");
-
-// Output
+val = Math.floor(Math.random() * 30 + 1);
 console.log(val);
-console.log(typeof val);
-// console.log(val.length); // now val has a length
-console.log(val.toFixed());
-
-// Type Coercion
-const val1 = String(5);
-const val2 = 7;
-
-const sum = Number(val1 + val2);
-
-console.log(sum);
-console.log(typeof sum);
