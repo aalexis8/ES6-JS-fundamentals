9b5f67fb97f834e24bd918281763a6a8729fde99
Alex Alexis <aalexis8@gmail.com>
2021-01-25 17:09:19 -0500

    different types in js primitive to reference

--- app.js
+++ app.js
@@ -1,70 +1,38 @@
-// defining your variables --
-// three available keywords -- var, let, const
-// let and const are newer, var has quirks you need to keep in mind.
+// Different types
 
-// creating a varibale
-// var name = "Alvin";
-// console.log(name);
-// name = "Kraven";
-// console.log(name);
+// PRIMITIVE
 
-// Initializing your vars, or else they are undefined.
-// var greeting;
-// console.log(greeting);
-// greeting = "What's good";
-// console.log(greeting);
+// String
+const name = "Jason Bourne";
 
-// rules for variable names -- letters, numbers, _, $
-// Cannot start with number
-// var _name = "Jason";
+// Number -- js doesn't differentiate number types, it is all numbers
+const age = 35;
 
-// // Multi word vars , camel case
-// var firstName = "John"; // Camel Case
-// var first_name = "Conners"; // underscore
-// var FirstName = "Eddie";// Pascal case
+// Boolean
+const hasKids = true;
 
-// Let and Scope, the block scoping variables
-// For now remember that let basically works like var --
-// We wont talk about scope yet.
+// Null
+const car = null;
 
-// LET
-// let name;
-// let name = "Alvin";
-// console.log(name);
-// name = "Kraven";
-// console.log(name);
+// Undefined
+let test;
 
-// CONST -- variables declared with const, can't be re-assigned a new value. They can't change value, once assigned. And they can't be declared without a value (can't do const name; needs a value)
+// Symbol
+const sym = Symbol();
 
-const name = "Anything even empty string";
-console.log(name);
-// cannot reassign
-// name = "Something new"; // not permitted
-// And require a value at declaration.
-//const greeting; //  Missing initializer in const declaration
+// REFERENCE TYPES -- brings back Objects
 
-// Keep in mind value inside a const object can change.
-// Take this object literal
-const person = {
-  name: "Jason",
-  age: 200,
+// Array
+const hobbies = ["Soccer", "Music", "Kite Flying"];
+
+// Object literal
+const address = {
+  city: "Columbus",
+  state: "OH",
 };
 
-// We can change the value of the key name or age
-person.name = "Voorheese";
-person.age = 300;
+const today = new Date();
 
-console.log(person); //
+console.log(today);
 
-const anotherperson = { name: "Freddy" };
-// can't assign person to a new or other person.
-// person = anotherperson;
-
-/* ---------- */
-const numbers = [1, 2, 3, 4, 5];
-// we can use the array methods to add items to this array
-numbers.push(6);
-console.log(numbers);
-
-// But we can't re-assign array to a new set of numbers
-numbers = [];
+console.log(typeof today);
