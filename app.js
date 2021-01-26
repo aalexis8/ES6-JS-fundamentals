b4825fe1cb603648bdb8967340374eb847a055f3
Alex Alexis <aalexis8@gmail.com>
2021-01-26 17:23:19 -0500

    Single element DOM selector

--- app.js
+++ app.js
@@ -1,45 +1,42 @@
-let val;
+// Single element DOM Selectors the two single -- element selectors
 
-val = document;
+// document.getElementById()
 
-// Working with the DOM -- be aware of various structures like
-// HTML collection which may look like an array but isn't an array per say. Can't use for each on an html collection
-// Nodelist on the other hand you can use for each on ... but is not exactly an array either
+// console.log(document.getElementById("task-title"));
 
-val = document.all; // returns an html collection
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+// Of course you can save task-title in a variable and save some typing
+// const taskTitle = document.getElementById("task-title");
+// Get elements' attributes
+// console.log(document.getElementById("task-title").id);
+// console.log(document.getElementById("task-title").className);
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+// Change Styling
+// document.getElementById("task-title").style.background = "#333";
+// document.getElementById("task-title").style.color = "#fff";
+// document.getElementById("task-title").style.padding = "5px";
+// taskTitle.style.background = "#333";
+// taskTitle.style.color = "#fff";
+// taskTitle.style.padding = "5px";
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
+// Change content
+// document.getElementById("task-title").textContent = "Task List";
+// document.getElementById("task-title").innerText = "My Task";
+// document.getElementById("task-title").innerHTML =
+//   '<span style="color:red"> Task List</span>';
+// taskTitle.textContent = "Task List";
+// taskTitle.innerText = "My Task";
+// taskTitleinnerHTML = '<span style="color:red"> Task List</span>';
 
-val = document.images;
+// The newer more powerful selector is querySelector()
+// Able to select by more than just id, takes any css selector
+// document.querySelector()
+console.log(document.querySelector("#task-title"));
+console.log(document.querySelector(".card-title"));
+console.log(document.querySelector("h5")); // returns the first h5 found
+document.querySelector("li").style.color = "red";
+document.querySelector("ul li").style.color = "blue";
 
-val = document.scripts;
-val = document.scripts[2].getAttribute("src");
-
-let scripts = document.scripts;
-
-let scriptsArr = Array.from(scripts);
-
-scriptsArr.forEach(function (script) {
-  console.log(script.getAttribute("src"));
-});
-
-console.log(val);
+document.querySelector("li:last-child").style.color = "red";
+document.querySelector("li:nth-child(3").style.color = "yellow";
+document.querySelector("li:nth-child(4)").textContent = "Hello World";
+document.querySelector("li:nth-child(odd)").style.background = "#ccc"; // only the first odd is changed, because this is a single selector
