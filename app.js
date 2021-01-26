29d8669744de16c19847db32ecce2c48ac4c5550
Alex Alexis <aalexis8@gmail.com>
2021-01-26 10:36:43 -0500

    function decalration, expressions, iife

--- app.js
+++ app.js
@@ -1,39 +1,82 @@
-const color = "green";
+// FUNCTION DECLARATIONS
+// function greet() {
+//   // console.log("Hello");
+//   return "Hello";
+// }
+// Now we console.log here at the function call
+// console.log(greet());
 
-switch (color) {
-  case "red":
-    console.log("Color is red");
-    break;
-  case "blue":
-    console.log("Color is blue");
-    break;
-  default:
-    console.log("Color is not red or blue");
-    break;
+//TAKING PARAMETERS (INPUT TO OUR FUNCTION)
+function greet(firstName) {
+  // now we can use the parameter/input in our function to do work
+  return "Hello " + firstName;
 }
 
-let day;
-switch (new Date().getDay()) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 2:
-    day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 4:
-    day = "Thursday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case 6:
-    day = "Saturday";
-    break;
+// MULTIPLE PARAMATERS
+function greet(firstName, lastName) {
+  // now we can use the parameter/input in our function to do work
+  return "Hello " + firstName + " " + lastName;
 }
-console.log(`Today is ${day}`);
+
+// DEFAULT VALUE PARAMETER, IN CASE IT IS CALLED WITHOUT A VALUE
+// BEFORE ES6
+// function greet(firstName, lastName) {
+//   if (typeof firstName === "undefined") {
+//     firstName = "John";
+//   }
+//   if (typeof lastName === "undefined") {
+//     lastName = "Dough";
+//   }
+//   return "Hello " + firstName + " " + lastName;
+// }
+
+// AFTER ES6 NOW DEFAULT VALUES CAN BE HANDLE LIKE THE FOLLOWING
+function greet(firstName = "John", lastName = "Dough") {
+  return "Hello " + firstName + " " + lastName;
+}
+
+// Calling without passing a value
+// console.log(greet());
+// Now we can pass in a value when calling the function
+// console.log(greet("Alvin", "Chipmunk"));
+
+// FUNCTION EXPRESSIONS -- a function as a variable assignment
+// In such situation usually the function is anonymous
+
+const cube = function (x) {
+  return x * x * x;
+};
+
+// console.log(cube(3));
+
+// IMMEDIATELY INVOKABLE FUNCTIONS EXPRESSIONS -- IIFEs (isn't that an E40 song lol) -- declare and run at once
+(function () {
+  console.log("if you dig it, I dug it");
+});
+
+// Nothing happens in the above function because we have no called the function yet. See below : we call it by adding another set of parenthesis
+// (function () {
+//   console.log("if you dig it, I dug it");
+// })();
+
+// can be parameterized like any other functions
+// (function (name) {
+//   console.log("my swisher " + name);
+// })("E40");
+
+// PROPERTY METHODS -- functions inside an object is called a method
+const todo = {
+  add: function () {
+    console.log("Add todo...");
+  },
+  edit: function (id) {
+    console.log(`Edit todo ${id}`);
+  },
+};
+
+todo.delete = function () {
+  console.log("Deleting todo...");
+};
+todo.add();
+todo.edit(34);
+todo.delete();
