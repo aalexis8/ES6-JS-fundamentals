e8af1a7121be8c946b6bf1ea41573b167d1e5032
Alex Alexis <aalexis8@gmail.com>
2021-01-26 16:28:56 -0500

    Looking at the DOM and exploring document.

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
-// Global Scope
-var a = 1;
-let b = 2;
-const c = 3;
+let val;
 
-// Now inside a function, function scope
-// function test() {
-//   var a = 6;
-//   let b = 7;
-//   const c = 8;
-//   console.log("Function Scope: ", a, b, c);
-// }
+val = document;
 
-// test();
+// Working with the DOM -- be aware of various structures like
+// HTML collection which may look like an array but isn't an array per say. Can't use for each on an html collection
+// Nodelist on the other hand you can use for each on ... but is not exactly an array either
 
-// Block Level Scope, anything inside of curly braces.
-// if (true) {
-//   // Block scope
-//   var a = 4; // var changes a globally here
-//   let b = 5;
-//   const c = 6;
-//   console.log("Block level: ", a, b, c);
-// } // let and const were added to allow level scoping. Fixing some of js weirdness
+val = document.all; // returns an html collection
+val = document.all[2];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
 
-// let and const have block level scope
-// while var has function scope -- that if it is not contained by a function it will be global
-console.log("Global Scope: ", a, b, c);
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+val = document.forms[0].action;
+
+val = document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+val = document.links[0].classList[0];
+
+val = document.images;
+
+val = document.scripts;
+val = document.scripts[2].getAttribute("src");
+
+let scripts = document.scripts;
+
+let scriptsArr = Array.from(scripts);
+
+scriptsArr.forEach(function (script) {
+  console.log(script.getAttribute("src"));
+});
+
+console.log(val);
