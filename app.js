9150661b125e8bf91e32ad077ba7b6d8dde82fbd
Alex Alexis <aalexis8@gmail.com>
2021-01-26 07:49:47 -0500

    switches another conditional structure

--- app.js
+++ app.js
@@ -1,95 +1,39 @@
-// If statements and comparison operators
-// if(condition is met) {
-//    do
-// } else {
-//   do other thing
-// }
-
-const id = 100;
-// let id;
-
-// EQUAL TO
-// if (id == 100) {
-//   console.log("CORRECT");
-// } else {
-//   console.log("INCORRECT");
-// }
-
-// NOT EQUAL TO
-// if (id != 101) {
-//   console.log("CORRECT");
-// } else {
-//   console.log("INCORRECT");
-// }
-
-// EQUAL TO VALUE AND TYPE
-// if (id === 100) {
-//   console.log("CORRECT");
-// } else {
-//   console.log("INCORRECT");
-// }
-
-// NOT EQUAL TO VALUE AND TYPE
-// if (id !== 100) {
-//   console.log("CORRECT");
-// } else {
-//   console.log("INCORRECT");
-// }
-
-// TESTING FOR UNDEFINED INCORRECT WAY, NOT FOUND
-// if (id) {
-//   console.log(`The ID is: ${id}`);
-// } else {
-//   console.log("NO ID");
-// } // BUT IT IS NOT FOUND IS DIFFERENT FROM UNDEFINED
-
-// THE CONTEXT OF WHAT HE IS TRYING TO ACCOMPLISH
-// if (typeof id !== "undefined") {
-//   console.log(`The ID is:  ${id}`);
-// } else {
-//   console.log("NO ID");
-// } // if id was declared and we have no value, we still get NO ID
-// So testing if a variable is non-existant as opposed to testing if a variable is undefined. I think that's worth pointing out.
-
-// GREATER THAN OR LESS THAN
-// if (id > 100) {
-//   console.log("CORRECT");
-// } else {
-//   console.log("INCORRECT");
-// }
-
-// IF ELSE
 const color = "green";
 
-// if (color === "vert") {
-//   console.log("Color is red");
-// } else if (color === "blue") {
-//   console.log("Color is blue");
-// } else {
-//   console.log("Color is not red or blue");
-// }
-
-// LOGICAL OPERATORS
-const name = "Smith";
-const age = 20;
-
-// AND &&
-if (age > 0 && age < 12) {
-  console.log(`${name} is a child`);
-} else if (age >= 13 && age <= 19) {
-  console.log(`${name} is a teenager`);
-} else {
-  console.log(`${name} is an adult`);
+switch (color) {
+  case "red":
+    console.log("Color is red");
+    break;
+  case "blue":
+    console.log("Color is blue");
+    break;
+  default:
+    console.log("Color is not red or blue");
+    break;
 }
 
-// OR ||
-if (age < 16 || age > 65) {
-  console.log(`${name} can not run in race`);
-} else {
-  console.log(`${name} is registered for the race`);
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
 }
-
-// TERNARY OPERATOR --
-console.log(id === 100 ? "CORRECT" : "INCORRECT");
-
-//
+console.log(`Today is ${day}`);
