7313607bb08cc4163caf3756e38017dd1b207139
Alex Alexis <aalexis8@gmail.com>
2021-01-25 20:04:54 -0500

    arrays and array methods

--- app.js
+++ app.js
@@ -1,42 +1,84 @@
-// Template literals, template strings part of es6, but compatible with all browsers.
+// Arrays & Array Methods
 
-// here some variables
-const name = "Doe";
-const age = 28;
-const job = "Software Developer";
-const city = "Arizona";
+// Create some arrays
+const numbers = [67, 34, 56, 73, 33, 44, 2];
 
-let html;
+// Creat using the array constructor
+const numbers1 = new Array(23, 45, 56, 78, 12);
 
-// Without template strings (es5)
-html =
-  "<ul><li>Name: " +
-  name +
-  "</li><li>Age: " +
-  age +
-  " </li><li>Job: " +
-  job +
-  "</li><li>City: " +
-  city +
-  " </li></ul>";
+// Of course you can have an array of strings
+const vegetables = ["zucchini", "broccoli", "Arugula", "Beets"];
 
-function hello() {
-  return "Hello";
+// Unlike other languages, you can mixe types inside an array
+const mixed = [25, "What's up", true, null, { a: 1, b: 2 }, new Date()];
+
+let val;
+
+// Get array length
+val = numbers.length;
+// Check if is array
+val = Array.isArray(numbers);
+
+// Return an index
+val = numbers[3];
+val = numbers[0];
+
+// Insert into array
+numbers[2] = 203;
+
+// Find index of value
+val = numbers.indexOf(203);
+
+// MUTATING ARRAYS
+// Add on to end
+numbers.push(207);
+
+// Add to front
+// numbers.unshift(207);
+// numbers.pop();
+// numbers.shift();
+
+// Reverse the array
+// numbers.reverse();
+
+// numbers.splice(2, 1);
+
+// Concat an array
+val = numbers.concat(numbers1);
+
+// Basic Sort
+val = vegetables.sort();
+val = numbers.sort();
+
+// To sort by value, you need compare the values
+// Currently it is sorting by index -- the default
+// Too early to intro arrow functions .. but i threw it in.
+// I would think I could omit the return keyword in this case.
+// val = numbers.sort((x, y) => {
+//   return x - y;
+// });
+
+val = numbers.sort(function (x, y) {
+  return x - y;
+});
+
+// Reverse of the above sort
+// val = numbers.sort((x, y) => {
+//   return y - x;
+// });
+
+val = numbers.sort(function (x, y) {
+  return y - x;
+});
+
+// Find
+function under50(num) {
+  return num < 50;
 }
 
-// With template literals, template strings, we can evaluate expressions and etc...
-html = `
-   <ul>
-    <li>Name: ${name}</li>
-    <li>Age: ${age}</li>
-    <li>Job: ${job}</li>
-    <li>City: ${city}</li>
-    <li>${2 + 5}</li>
-    <li>${hello()}</li>
-    <li>${age > 30 ? "Over 30" : "Under 30"}</li>
-   </ul>
-
-`;
-
-// then add this html to the DOM
-document.body.innerHTML = html;
+function over50(num) {
+  return num > 50;
+}
+val = numbers.find(over50);
+console.log(val);
+console.log(numbers);
