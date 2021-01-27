4d59c8a7c0787b1779d0aad96fb313bb40083e5d
Alex Alexis <aalexis8@gmail.com>
2021-01-27 10:36:48 -0500

    Traversing the DOM think of it as traversing Tree

--- app.js
+++ app.js
@@ -1,63 +1,59 @@
-// document.getElementsByClassName
+// Traversing the DOM, Moving through the tree, branches children siblings
 
-// const items = document.getElementsByClassName("collection-item");
+let val;
+const list = document.querySelector("ul.collection");
+const listItem = document.querySelector("li.collection-item:first-child");
 
-// console.log(items);
-// console.log(items[0]);
+val = listItem;
+val = list;
 
-// items[0].style.color = "red";
-// items[3].style.textContent = "Hello";
+// Get child nodes (a nodelist of all the child nodes.)
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[0].nodeType;
 
-// const listItems = document
-//   .querySelector("ul")
-//   .getElementsByClassName("collection-item");
+// NodeType guide, glossary for numbers returned by nodeType
+// 1 - Element
+// 2 - Attribute (deprecated)
+// 3 - Text node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype
 
-// console.log(listItems);
+// Get children element nodes
+val = list.children;
+val = list.children[1];
+list.children[1].textContent = "Hello";
 
-// document.getElementsByTagName
+// Children of children
+list.children[3].children[0].id = "test-link";
+val = list.children[3].children[0];
 
-// let lis = document.getElementsByTagName("li");
+// First Child
+val = list.firstChild;
+val = list.firstElementChild; // specify to avoid text nodes
 
-// console.log(lis);
-// console.log(lis[0]);
+// Last Child
+val = list.lastChild;
+val = list.lastElementChild;
 
-// lis[0].style.color = "red";
-// lis[3].textContent = "Hello";
+// Count child elements
+val = list.childElementCount;
 
-// // Convert HTML Collection into array
-// lis = Array.from(lis);
+// Get parent node
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
 
-// lis.reverse();
+// Get next sibling
+val = listItem.nextSibling;
+val = listItem.nextElementSibling;
+val = listItem.nextElementSibling.nextElementSibling;
+val = listItem.nextElementSibling.previousElementSibling;
 
-// lis.forEach(function (li, index) {
-//   console.log(li.className);
-//   li.textContent = `${index}: Hello `; //            "Hello";
-// });
+//Get previous sibling
+val = listItem.previousSibling;
+val = listItem.previousElementSibling;
 
-// console.log(lis);
-
-// document.querySelectorAll -- returns a node list forEach adjacent
-// NodeList don't need to be converted into an array.
-const items = document.querySelectorAll("ul.collection li.collection-item");
-
-items.forEach(function (item, index) {
-  // console.log(item.className);
-  item.textContent = `${index}: Hello `;
-});
-
-const liOdd = document.querySelectorAll("li:nth-child(odd)");
-const liEven = document.querySelectorAll("li:nth-child(even)");
-
-liOdd.forEach(function (li, index) {
-  li.style.background = "#ccc";
-});
-
-for (let i = 0; i < liEven.length; i++) {
-  liEven[i].style.background = "#f4f4f4";
-}
-
-// Just wanted to check that this works here also
-for (const li of liEven) {
-  li.style.background = "red";
-}
-console.log(items);
+console.log(val);
