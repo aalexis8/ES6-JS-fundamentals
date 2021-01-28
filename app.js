5df375a4e373f0ce17d4be4d06d32cfc5c366380
Alex Alexis <aalexis8@gmail.com>
2021-01-27 19:33:34 -0500

    local vs session storage

--- app.js
+++ app.js
@@ -1,49 +1,59 @@
-// EVENT BUBBLING
-// document.querySelector(".card-title").addEventListener("click", function () {
-//   console.log("card-title");
+// Local & Session Storage, local storage data stays until user clears it out. Session storage is remove once the browser is closed.
+
+// set local storage
+// localStorage.setItem("name", "Alex");
+// localStorage.setItem("age", 40);
+
+// set session storage
+// sessionStorage.setItem("name", "Alexis");
+
+// get from storage
+// const name = localStorage.getItem("name");
+// const age = localStorage.getItem("age");
+
+// localStorage.removeItem("name");
+// const name = localStorage.getItem("name");
+
+// localStorage.clear();
+// console.log(name, age);
+
+// But this erase the previous item -- if you try to add another task
+// document.querySelector("form").addEventListener("submit", function (e) {
+//   const task = document.getElementById("task").value;
+//   localStorage.setItem("task", task);
+//   alert("Saved!!");
+
+//   e.preventDefault();
 // });
 
-// document.querySelector(".card-content").addEventListener("click", function () {
-//   console.log("card-content");
-// });
+// Fixing that by creating an array of string to store things in as needed
+document.querySelector("form").addEventListener("submit", function (e) {
+  const task = document.getElementById("task").value;
 
-// document.querySelector(".card").addEventListener("click", function () {
-//   console.log("card");
-// });
+  let tasks; // our array variable
 
-// document.querySelector(".col").addEventListener("click", function () {
-//   console.log("col");
-// });
-
-// EVENT DELEGATION
-
-// const delItem = document.querySelector(".delete-item");
-
-// delItem.addEventListener("click", deleteItem);
-
-document.body.addEventListener("click", deleteItem);
-
-// function deleteItem(e) {
-//   if (e.target.className === "fa fa-remove") {
-//     console.log("item deleted");
-//   }
-// }
-
-// function deleteItem(e) {
-//   if (e.target.parentElement.className === "delete-item secondary-content") {
-//     console.log("item deleted");
-//   }
-// }
-
-// function deleteItem(e) {
-//   if (e.target.parentElement.classList.contains("delete-item")) {
-//     console.log("item deleted");
-//   }
-// }
-
-function deleteItem(e) {
-  if (e.target.parentElement.classList.contains("delete-item")) {
-    console.log("item deleted");
-    e.target.parentElement.parentElement.remove();
+  if (localStorage.getItem("tasks") === null) {
+    tasks = []; // create our empty array and await items
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
   }
-}
+
+  // we are adding items to tasks
+  tasks.push(task);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  alert("Saved!!");
+
+  e.preventDefault();
+});
+
+// Trying to read from that array keep in mind the need to parse
+const tasks = JSON.parse(localStorage.getItem("tasks"));
+
+// tasks.forEach((element) => {
+//   console.log(element);
+// });
+
+tasks.forEach(function (task) {
+  console.log(task);
+});
